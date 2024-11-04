42662e5456a52b1d675f71ec3a626dd1807960b0
Alikh-collab <chort445@gmail.com>
2024-11-04 13:03:10 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+// tapsyrma 1
+
+let slicesPerPizza = 8;
+
+let numberOfpizza = 5;
+
+var totalSlices = slicesPerPizza * numberOfpizza;
+
+
+console.log("tapsyrma 1", totalSlices);
+
+
+
+
+// tapsyrma 2
+
+let totalChocolates = 24;
+
+let numberOfFriends = 6;
+
+var chocolatesOfFriend = totalChocolates / numberOfFriends;
+
+
+console.log("tapsyrma 2", chocolatesOfFriend);
+
+
+
+
+// tapsyrma 3
+planHeight = 10;
+
+planHeight += 2;
+
+
+console.log("tapsyrma 3", planHeight);
